22569e1131c5a907f787189349361872840d4f09
Lucas <lucass@gmail.com>
2016-09-05 23:17:17 -0300

    Partial commit...to be continued

--- src/Stochastic/AdaptativeRandomSearch.hs
+++ src/Stochastic/AdaptativeRandomSearch.hs
@@ -1,11 +1,27 @@
 module Stochastic.AdaptativeRandomSearch
 (
   adaptativeRandomSearch
-)where
+)
+where
 import System.Random
 
 adaptativeRandomSearch :: Double -> Double -> Int -> Int -> Double -> Double -> Double -> Double -> Double -> ([Double] -> Double) -> StdGen -> [Double]
 adaptativeRandomSearch 
+  min 
+  max 
+  searchSpace 
+  iterationCount 
+  initialFactor 
+  stepFactor
+  smallStepFactor 
+  largeStepFactor 
+  noChangeMax
+  costFunction 
+  rndgen = [0]
+
+searchBest :: Int -> Candidate -> Double -> Double -> Int -> Int -> Double -> Double -> Double -> Double -> Double -> ([Double] -> Double) -> StdGen -> [Double]
+  iterationCount 
+  best
   min 
   max 
   searchSpace 
