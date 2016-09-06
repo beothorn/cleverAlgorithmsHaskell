ee798241133004f182c048ddda780b8856d8d715
Lucas <lucass@gmail.com>
2016-09-05 22:07:37 -0300

    Moving functions around to more apropriate places

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
 import Stochastic.RandomSearch
+import System.Random
+
+objective_function :: [Double] -> Double
+objective_function nvect = sum $ map (**2) nvect
 
 main :: IO ()
-main = result
+main = do
+  print "Random search" 
+  g <- newStdGen
+  print (randomSearch (-5) 5 100 objective_function g)
